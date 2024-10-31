3977090184877a3c276de67fc9e1b364b6e9f9cc
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-10-31 14:09:48 -0400

    hero section ready: desktop and mobile

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,10 @@ import Navbar from "@/components/Navbar";
 function Home() {
   return (
     <div className="mx-[7.2%]">
+      <div className="fixed inset-y-0 left-[50%] w-px border-r border-dashed border-gray-300 -z-10"></div>
+      <div className="fixed inset-y-0 left-[28.5%] w-px border-r border-dashed border-gray-300 -z-10"></div>
+      <div className="fixed inset-y-0 left-[71.5%] w-px border-r border-dashed border-gray-300 -z-10"></div>
+
       <Navbar />
       <section className="-mt-[64px]">
         <Hero />
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,13 +11,16 @@ const Hero = () => {
           John Penafiel
         </h1>
       </div>
-      <div className="flex items-top justify-end pt-10 mr-[13.1%] text-lg font-normal text-gray-500">
-        <div className="border-l border-gray-400 h-6 mr-4"></div>
-        <div className="text-gray-600">
+      <div className="flex items-top pt-10 text-lg font-normal text-gray-500">
+        <div className="flex ml-[75.11%] w-px border-l border-gray-400 h-6 mr-4"></div>
+        <div className="flex flex-col text-gray-600">
           <p>I solve, I create.</p>
-          <a href="#about" className="underline hover:text-gray-900">
-            More about me →
-          </a>
+          <div className="flex">
+            <a href="#about" className="underline hover:text-gray-900">
+              More about me
+            </a>
+            <div className="px-2">→</div>
+          </div>
         </div>
       </div>
     </div>
