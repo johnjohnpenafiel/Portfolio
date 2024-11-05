f36e5b5d9b98d392de2aeaf6b023a53ae094a56f
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-11-05 02:27:28 -0500

    updated mobile navbar animation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,12 +8,14 @@ import { FiMenu, FiX } from "react-icons/fi";
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const MenuItems = ["Work", "About", "CV"];
+
   const toggleMenu = () => {
     setIsMenuOpen((prev) => !prev);
   };
 
   return (
-    <nav className="relative py-3 px-6 || border-b border-gray-200/80 bg-white">
+    <nav className="relative py-3 px-7 md:px-5 || border-b border-gray-200/80 bg-white">
       <div className="flex items-center justify-between">
         <Image
           src="/images/logo4.png"
@@ -31,47 +33,39 @@ const Navbar = () => {
 
         {/* Desktop Menu */}
         <ul className="hidden md:flex space-x-12">
-          <li>
-            <a href="#" className="text-gray-500 hover:text-gray-900 text-base">
-              Work
-            </a>
-          </li>
-          <li>
-            <a href="#" className="text-gray-500 hover:text-gray-900 text-base">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#" className="text-gray-500 hover:text-gray-900 text-base">
-              CV
-            </a>
-          </li>
+          {MenuItems.map((menuItem) => (
+            <li key={menuItem}>
+              <a
+                href="#"
+                className="text-gray-500 hover:text-gray-900 text-base"
+              >
+                {menuItem}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
 
       {/* Mobile Menu */}
       <ul
-        className={`absolute flex flex-col top-full left-0 w-full md:hidden items-center space-y-2 py-4 || bg-white border-b border-gray-200/80 transform transition-transform origin-top duration-500 ease-out ${
+        className={`absolute flex flex-col top-full left-0 w-full md:hidden items-center space-y-2 py-4 || bg-white border-b border-gray-200/80 transition-transform origin-top duration-500 ease-out ${
           isMenuOpen
             ? "scale-y-100 pointer-events-auto"
             : "scale-y-0 pointer-events-none"
         }`}
       >
-        <li>
-          <a href="#" className="text-gray-500 hover:text-gray-900 text-2xl">
-            Work
-          </a>
-        </li>
-        <li>
-          <a href="#" className="text-gray-500 hover:text-gray-900 text-2xl">
-            About
-          </a>
-        </li>
-        <li>
-          <a href="#" className="text-gray-500 hover:text-gray-900 text-2xl">
-            CV
-          </a>
-        </li>
+        {MenuItems.map((menuItem) => (
+          <li key={menuItem}>
+            <a
+              href="#"
+              className={`text-gray-500 hover:text-gray-900 text-2xl transition-opacity ease-in delay-300 duration-250 ${
+                isMenuOpen ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              {menuItem}
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
