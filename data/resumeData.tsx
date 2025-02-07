993987640347f389b5218452b28601fcb9d35b40
John John Penafiel <dev.johnpenafiel@gmail.com>
2025-02-06 23:25:51 -0500

    Update: data information

--- data/resumeData.tsx
+++ data/resumeData.tsx
@@ -1,2 +1,2 @@
 export const resumeLink: string =
-  "https://drive.google.com/file/d/1hc8hgwc091PLfAIz54zO3sT3dOQL9Abp/view?usp=sharing";
+  "https://drive.google.com/file/d/1-jiijXOOlLsP0HeHUDkSyH2K0CJ2OgnT/view?usp=sharing";
